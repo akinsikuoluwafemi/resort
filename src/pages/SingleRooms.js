72e48d1ab571e5cb9f844a5e6aa4ddddecb6915d
Akinsiku Femi <akinsiku13@gmail.com>
2020-06-29 08:23:18 +0100

    Add starter files and components

---
+++ src/pages/SingleRooms.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+ class SingleRooms extends Component {
+    render() {
+        return (
+            <div>
+               Hello from singlular room page
+            </div>
+        )
+    }
+}
+
+export default SingleRooms;
