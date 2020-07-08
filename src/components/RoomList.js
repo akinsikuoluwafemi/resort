c9b840f50ab45f4ea41bb666887a8317de2e3cfd
Akinsiku Femi <akinsiku13@gmail.com>
2020-07-08 11:14:06 +0100

    Implement the function for get unique room types

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import Room from './Room';
+import Rooms from '../pages/Rooms';
 
-export default function RoomsList() {
+
+
+
+export default function RoomsList({rooms}) {
+    if(rooms.length === 0){
+        return (
+            <div className="empty-search">
+                <h3>unfortunately no rooms match your search parameter</h3>
+            </div>
+        )
+       
+    }
     return (
-        <div>
-            hello from roomlist
-        </div>
+        <section className="roomslist">
+            <div className="roomslist-center">
+                {
+                rooms.map(item => {
+                    return <Room key={item.id} room={item}/>
+                })
+            }
+            </div>
+        </section>
     )
 }
--- src/context.js
+++ src/context.js
@@ -11,20 +11,38 @@ const RoomContext = React.createContext();
          rooms: [],
          sortedRooms: [],
          featuredRooms: [],
-         loading: true
+         loading: true,
+         type: 'all',
+         capacity: 1,
+         price: 0,
+         minPrice: 0,
+         minSize: 0,
+         maxSize: 0,
+         breakfast: false,
+         pets: false
 
      };
     //  getData
      
+     
      componentDidMount() {
          let rooms = this.formatData(items);
          let featuredRooms = rooms.filter(room => room.featured === true)
+         let maxPrice = Math.max(...rooms.map(item => item.price))
+         let maxSize = Math.max(...rooms.map(item => item.size))
+
          this.setState({
              rooms,
              featuredRooms,
              sortedRooms: rooms,
-             loading: false
+             loading: false,
+             price: maxPrice,
+             maxPrice,
+             maxSize
+
          })
+        //  console.log(this.state)
+        //  console.log(rooms)
      }
 
     formatData(items){
@@ -47,9 +65,22 @@ const RoomContext = React.createContext();
      }
 
 
+     handleChange = event => {
+         const type = event.target.type
+         const name = event.target.name
+         const value = event.target.value
+         console.log(type, name, value);
+
+     }
+
+     filterRooms = () => {
+         console.log('hello')
+     }
+
+
      render(){
          return (
-             <RoomContext.Provider value={{...this.state, getRoom: this.getRoom }}>
+             <RoomContext.Provider value={{...this.state, getRoom: this.getRoom, handleChange: this.handleChange }}>
                 {this.props.children}
             </RoomContext.Provider>
         )
@@ -58,4 +89,19 @@ const RoomContext = React.createContext();
 export default RoomProvider;
 
 const RoomConsumer = RoomContext.Consumer;
+
+
+export function withRoomConsumer(Component){
+    return function ConsumerWrapper(props) {
+        return <RoomConsumer>
+            {value => <Component {...props} context={value} />}
+        </RoomConsumer>
+    }
+        
+}
+
+
+
+
 export { RoomProvider, RoomConsumer, RoomContext };
+
